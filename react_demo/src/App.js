fd309684a02899b72683649a8d7c41a28d89307e
BryanMgbeojirikwe <73403625+BryanMgbeojirikwe@users.noreply.github.com>
2023-12-04 22:50:18 -0500

    demo ready

--- react_demo/src/App.js
+++ react_demo/src/App.js
@@ -1,46 +1,70 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import GameCard from "./GameCard";
-import GameModal from "./GameModal"; // Make sure you have this component for displaying screenshots
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import GameCard from './GameCard';
+import GameModal from './GameModal';
 
 function App() {
-  const API_KEY = "27fffb70989a4131a61a960c34e3cd46";
+  const API_KEY = '27fffb70989a4131a61a960c34e3cd46';
   const API_URL = `https://api.rawg.io/api/games?key=${API_KEY}`;
-  const API_SEARCH = `https://api.rawg.io/api/games?key=${API_KEY}&search=`;
-
   const [games, setGames] = useState([]);
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState('');
   const [selectedGameScreenshots, setSelectedGameScreenshots] = useState([]);
+  const [gameSummary, setGameSummary] = useState('');
 
   useEffect(() => {
     fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setGames(data.results));
+      .then(res => res.json())
+      .then(data => {
+        setGames(data.results);
+      });
   }, []);
 
   const handleSearch = (e) => {
     e.preventDefault();
-    if (term === "") {
-      window.location.reload();
-    } else {
-      fetch(API_SEARCH + term)
-        .then((res) => res.json())
-        .then((data) => {
+    if (term) {
+      fetch(`${API_URL}&search=${term}`)
+        .then(res => res.json())
+        .then(data => {
           setGames(data.results);
-          setTerm("");
+          setTerm('');
         });
     }
   };
 
-  const onImageClick = (game) => {
-    const screenshotsUrl = `https://api.rawg.io/api/games/${game.id}/screenshots?key=${API_KEY}`;
-    
+  const onImageClick = (gameId) => {
+    const screenshotsUrl = `https://api.rawg.io/api/games/${gameId}/screenshots?key=${API_KEY}`;
+    const gameDetailsUrl = `https://api.rawg.io/api/games/${gameId}?key=${API_KEY}`;
+
     fetch(screenshotsUrl)
       .then(res => res.json())
       .then(data => {
-        setSelectedGameScreenshots(data.results); // Update state with fetched screenshots
+        setSelectedGameScreenshots(data.results);
       })
       .catch(error => console.error('Error fetching screenshots:', error));
+
+    fetch(gameDetailsUrl)
+      .then(res => res.json())
+      .then(data => {
+        setGameSummary(data.description_raw); // Use the raw description without HTML tags
+      })
+      .catch(error => console.error('Error fetching game details:', error));
+
+    document.body.classList.add('modal-open');
+  };
+
+  const closeModal = () => {
+    document.body.classList.remove('modal-open');
+    setSelectedGameScreenshots([]);
+    setGameSummary('');
+  };
+
+  const fetchGamesByGenre = (genreId) => {
+    const genreUrl = `https://api.rawg.io/api/games?key=${API_KEY}&genres=${genreId}`;
+    fetch(genreUrl)
+      .then(res => res.json())
+      .then(data => {
+        setGames(data.results);
+      });
   };
 
   return (
@@ -48,6 +72,13 @@ function App() {
       <div className="search_nav">
         <div className="title">
           <h1 onClick={() => window.location.reload()}>GameNest</h1>
+        </div>
+        <div className="genre_buttons">
+          <button onClick={() => fetchGamesByGenre(4)}>Action</button>
+          <button onClick={() => fetchGamesByGenre(3)}>Adventure</button>
+          <button onClick={() => fetchGamesByGenre(5)}>RPG</button>
+          <button onClick={() => fetchGamesByGenre(7)}>Puzzle</button>
+          <button onClick={() => fetchGamesByGenre(1)}>Racing</button>
         </div>
         <div className="search_box">
           <form onSubmit={handleSearch}>
@@ -63,15 +94,16 @@ function App() {
       </div>
 
       <div className="games">
-        {games.map(game => (
-          <GameCard key={game.id} game={game} onImageClick={onImageClick} />
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} onImageClick={() => onImageClick(game.id)} />
         ))}
       </div>
 
       {selectedGameScreenshots.length > 0 && (
         <GameModal
           screenshots={selectedGameScreenshots}
-          closeModal={() => setSelectedGameScreenshots([])}
+          summary={gameSummary}
+          closeModal={closeModal}
         />
       )}
     </div>
--- react_demo/src/GameModal.js
+++ react_demo/src/GameModal.js
@@ -1,32 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './GameModal.css';
 
-const GameModal = ({ screenshots, closeModal }) => {
+const GameModal = ({ screenshots, summary, closeModal }) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
+  // Function to remove HTML tags from summary
+  const cleanSummary = (text) => {
+    return text.replace(/<\/?[^>]+(>|$)/g, "");
+  };
+
+  // Function to update the index and reset the animation
+  const updateIndex = (newIndex) => {
+    const screenshotElement = document.querySelector('.modal-screenshot');
+    screenshotElement.style.animation = 'none'; // Reset animation
+    setTimeout(() => { // Re-apply the animation
+      setCurrentImageIndex(newIndex);
+      screenshotElement.style.animation = '';
+    }, 10); // Timeout to allow animation reset
+  };
+
   const showPrevious = () => {
-    setCurrentImageIndex(prev => prev > 0 ? prev - 1 : 0);
+    const newIndex = currentImageIndex > 0 ? currentImageIndex - 1 : 0;
+    updateIndex(newIndex);
   };
 
   const showNext = () => {
-    setCurrentImageIndex(prev => prev < screenshots.length - 1 ? prev + 1 : screenshots.length - 1);
+    const newIndex = currentImageIndex < screenshots.length - 1 ? currentImageIndex + 1 : screenshots.length - 1;
+    updateIndex(newIndex);
   };
+
+  
 
   return (
     <div className="modal-overlay" onClick={closeModal}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
-        {screenshots.length > 0 && (
-          <div className="image-slider">
-            <button className="slide-btn left" onClick={showPrevious}>&lt;</button>
-            <img
-              src={screenshots[currentImageIndex].image}
-              alt={`Screenshot ${currentImageIndex + 1}`}
-              className="modal-screenshot"
-            />
-            <button className="slide-btn right" onClick={showNext}>&gt;</button>
-          </div>
-        )}
-        <button className="close-modal" onClick={closeModal}>Close</button>
+        <div className="image-slider">
+          <button className="slide-btn left" onClick={showPrevious}>&lt;</button>
+          <img
+            src={screenshots[currentImageIndex].image}
+            alt={`Screenshot ${currentImageIndex + 1}`}
+            className="modal-screenshot"
+          />
+          <button className="slide-btn right" onClick={showNext}>&gt;</button>
+        </div>
+        <p className="game-summary">{cleanSummary(summary)}</p>
       </div>
     </div>
   );
