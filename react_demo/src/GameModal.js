3bb5921121587bc93967c3367aade5a1ab82c386
BryanMgbeojirikwe <73403625+BryanMgbeojirikwe@users.noreply.github.com>
2023-12-04 21:47:23 -0500

    Screenshots version 2

--- react_demo/src/GameModal.js
+++ react_demo/src/GameModal.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import './GameModal.css'; // Ensure you have CSS for styling
+import React, { useState } from 'react';
+import './GameModal.css';
 
 const GameModal = ({ screenshots, closeModal }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const showPrevious = () => {
+    setCurrentImageIndex(prev => prev > 0 ? prev - 1 : 0);
+  };
+
+  const showNext = () => {
+    setCurrentImageIndex(prev => prev < screenshots.length - 1 ? prev + 1 : screenshots.length - 1);
+  };
+
   return (
     <div className="modal-overlay" onClick={closeModal}>
       <div className="modal-content" onClick={e => e.stopPropagation()}>
-        <div className="image-gallery">
-          {screenshots.map((screenshot, index) => (
-            <img key={index} src={screenshot.image} alt={`Screenshot ${index + 1}`} />
-          ))}
-        </div>
+        {screenshots.length > 0 && (
+          <div className="image-slider">
+            <button className="slide-btn left" onClick={showPrevious}>&lt;</button>
+            <img
+              src={screenshots[currentImageIndex].image}
+              alt={`Screenshot ${currentImageIndex + 1}`}
+              className="modal-screenshot"
+            />
+            <button className="slide-btn right" onClick={showNext}>&gt;</button>
+          </div>
+        )}
         <button className="close-modal" onClick={closeModal}>Close</button>
       </div>
     </div>
